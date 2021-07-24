4b94ba3daf97b3b0fa14f797e1a004303521e387
jan <bossjien26@gmail.com>
2021-07-24 15:52:12 +0800

    create middleware

---
+++ src/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace src.Middlewares
+{
+    public class AuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthenticationMiddleware(RequestDelegate next) => _next = next;
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var path = httpContext.Request.Path;
+            if (path.HasValue && path.Value.StartsWith("/admin"))
+            {
+                if (httpContext.Session.GetString("username") == null)
+                {
+                    httpContext.Response.Redirect("/login/index");
+                }
+            }
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AuthenticationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAuthenticationMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthenticationMiddleware>();
+        }
+    }
+}
