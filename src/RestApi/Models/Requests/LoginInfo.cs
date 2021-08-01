b19f4a94f513ef1dda39b049134c7d5ec723b3cf
Jan <bossjien26@gmail.com>
2021-08-01 19:07:10 +0800

    feat:model add column

--- src/RestApi/Models/Requests/LoginInfo.cs
+++ src/RestApi/Models/Requests/LoginInfo.cs
@@ -2,6 +2,7 @@ namespace ResApi.src.Models
 {
     public class LoginInfo
     {
+        public int Id { get; set; }
         public string Mail { get; set; }
         public string Password { get; set; }
     }
