308b731390f2655d7d818226d26fd7234429de0f
Jan <bossjien26@gmail.com>
2021-08-02 21:11:22 +0800

    feat:model add required type

--- src/RestApi/Models/Requests/LoginInfo.cs
+++ src/RestApi/Models/Requests/LoginInfo.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ResApi.src.Models
 {
     public class LoginInfo
     {
         public int Id { get; set; }
+
+        [Required]
         public string Mail { get; set; }
+        
+        [Required]
         public string Password { get; set; }
     }
 }
