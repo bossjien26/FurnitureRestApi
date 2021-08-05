74f8ec3e60e7f99dc8519af10d5d0b9cd4a9b9f2
Jan <bossjien26@gmail.com>
2021-08-05 23:51:15 +0800

    feat:create product entify

---
+++ src/Entities/Product.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Sequence { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime RelateAt { get; set; }
+
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+
+        public bool IsDisplay { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+        public virtual ICollection<ProductSpecification> ProductSpecifications { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+    }
+}
