c392a651ccafa2e7ed1a9ecf3cc95bb2972e4171
Jan <bossjien26@gmail.com>
2021-08-06 21:49:58 +0800

    feat:create orderProduct entity

---
+++ src/Entities/OrderProduct.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    [Table("OrderProduct")]
+    public class OrderProduct
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(256)]
+        public string ProductName { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(256)]
+        public string Specification { get; set; }
+
+        public int Quality { get; set; }
+
+        public int Price { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
