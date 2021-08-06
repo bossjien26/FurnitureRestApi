527ad652b223048e084d1366aafa05ff28d3230c
Jan <bossjien26@gmail.com>
2021-08-06 23:20:16 +0800

    feat:add entity

--- src/Repositories/DbContextEntity.cs
+++ src/Repositories/DbContextEntity.cs
@@ -10,7 +10,39 @@ namespace DbEntity
         public DbContextEntity(DbContextOptions<DbContextEntity> options)
         : base(options) { }
 
+        #region Order
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderPay> OrderPays { get; set; }
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        #endregion
+
+        #region Product
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductSpecification> ProductSpecifications { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Specification> Specifications { get; set; }
+
+        public DbSet<SpecificationContent> SpecificationContents { get; set; }
+
+        #endregion
+
+        #region user
+
         public DbSet<User> Users { get; set; }
+
+        public DbSet<UserDetail> UserDetails { get; set; }
+
+        #endregion
 
         public async void BeginTransaction()
         {
