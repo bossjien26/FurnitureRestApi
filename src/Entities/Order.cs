19ec226639c66dcc7450404cd6ea4a330778a5d2
Jan <bossjien26@gmail.com>
2021-08-06 22:39:34 +0800

    feat:order status add default value to order status enum processing

--- src/Entities/Order.cs
+++ src/Entities/Order.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Enum;
 
 namespace Entities
 {
@@ -14,7 +15,7 @@ namespace Entities
         [ForeignKey("User")]
         public int UserId { get; set; }
 
-        public byte Status { get; set; }
+        public byte Status { get; set; } = (int)OrderStatus.Processing;
 
         [Column(TypeName = "Varchar")]
         [StringLength(150)]
