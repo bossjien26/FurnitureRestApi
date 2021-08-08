e3a02981b1db08ff23cb26ef22d33737bdc08af9
Jan <bossjien26@gmail.com>
2021-08-08 18:53:26 +0800

    feat:add insertMany method

--- src/Repositories/IRepository/IGenericRepository.cs
+++ src/Repositories/IRepository/IGenericRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -15,6 +16,8 @@ namespace src.Repositories.IRepository
         IQueryable<T> GetAll();
         
         Task Insert(T entity);
+
+        Task InsertMany(List<T> entity);
         
         Task MultipleInsert(T entity);
         
