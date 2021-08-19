899b51a94c4e40551ac9a3e484a4d0ca07b29087
jan <bossjien26@gmail.com>
2021-08-19 21:33:35 +0800

    test:add user controller test

--- tests/RestApi.Test/Controllers/UserControllerTest.cs
+++ tests/RestApi.Test/Controllers/UserControllerTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -5,7 +7,10 @@ using Moq;
 using NUnit.Framework;
 using RestApi.src.Controllers;
 using RestApi.src.Models;
+using RestApi.Test.DatabaseSeeders;
 using RestApi.Test.Repositories;
+using src.Repositories.IRepository;
+using src.Repositories.Repository;
 
 namespace RestApi.Test.Controllers
 {
@@ -13,12 +18,16 @@ namespace RestApi.Test.Controllers
     public class UserControllerTest : BaseRepositoryTest
     {
         private readonly UserController _controller;
+
+        private readonly IUserRepository _repository;
+
         public UserControllerTest()
         {
             _controller = new UserController(
                 _context,
                 new Mock<ILogger<UserController>>().Object,
                 new Mock<AppSettings>().Object);
+            _repository = new UserRepository(_context);
         }
 
         [Test]
@@ -26,8 +35,60 @@ namespace RestApi.Test.Controllers
         {
             //Arrange &  Act
             var authenticateRequest = new AuthenticateRequest(){Mail = "adf",Password = "fsd"};
-            //Act
+            //Assert
             var result = _controller.Authenticate(authenticateRequest);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public void ShouldShowUser()
+        {
+            //Arrange & Act & Assert
+            var result = _controller.ShowUser();
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public void ShouldUpdateUserIsNull()
+        {
+            //Arrange & Act & Assert
+            var result = _controller.UpdateUser(null);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task ShouldUpdateUser()
+        {
+            //Arrange
+            var testData = UserSeeder.SeedOne();
+            await _repository.Insert(testData);
+            testData.IsDelete = true;
+
+            //Act
+            var result = _controller.UpdateUser(testData);
+
+            //Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task ShouldInsertUserIsNUll()
+        {
+            //Arrange & Act & Assert
+            var result = await _controller.InsertUser(null);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task ShouldInsertUser()
+        {
+            //Arrange
+            var testData = UserSeeder.SeedOne();
+
+            //Act
+            var result = await _controller.InsertUser(testData);
+
+            //Assert
             Assert.IsInstanceOf<OkObjectResult>(result);
         }
     }
