2a7653cf3d6bf27c81913848e97dea71e314eb45
jan <bossjien26@gmail.com>
2021-08-20 21:11:46 +0800

    feat:add JwtToken class

---
+++ src/Middlewares/Authentication/JwtToken.cs
@@ -0,0 +1,11 @@
+namespace Middlewares.Authentication
+{
+    public class JwtToken
+    {
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+    }
+}
