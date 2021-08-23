a018117396c8454bba184a67402feba686a99988
jan <bossjien26@gmail.com>
2021-08-23 23:21:11 +0800

    feat:add smtp setting

---
+++ src/Helpers/Mailer.cs
@@ -0,0 +1,13 @@
+namespace Helpers
+{
+    public class Mailer
+    {
+        public string MailTo { get; set; }
+
+        public string NameTo { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ src/Helpers/SmtpMailConfig.cs
@@ -0,0 +1,21 @@
+namespace Helpers
+{
+    public class SmtpMailConfig
+    {
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public int Post { get; set; }
+
+        public string Server { get; set; }
+
+        public string FromAddress { get; set; }
+
+        public string FromDisplayName { get; set; }
+
+        public bool EnableSsl { get; set; }
+
+        public string TextPart { get; set; }
+    }
+}
