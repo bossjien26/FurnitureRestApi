48407bb2c944279a89582f4bd052fa11af3400a2
jan <bossjien26@gmail.com>
2021-08-24 22:48:05 +0800

    feat:add register method

--- src/RestApi/Controllers/UserController.cs
+++ src/RestApi/Controllers/UserController.cs
@@ -17,6 +17,8 @@ using System.Threading.Tasks;
 using Middlewares.Authentication;
 using Enum;
 using RestApi.Models.Requests;
+using Services.IService;
+using Services.Service;
 
 namespace RestApi.src.Controllers
 {
@@ -30,15 +32,21 @@ namespace RestApi.src.Controllers
 
         private readonly AppSettings _appSettings;
 
+        private readonly MailHelper _mailHelper;
+
+        private readonly IUserDetailService _userDetailService;
+
         public UserController(DbContextEntity context, ILogger<UserController> logger,
-         AppSettings appsetting)
+         AppSettings appsetting, MailHelper mailHelper, UserDetailService userDetailService)
         {
             _repository = new UserService(context);
             _logger = logger;
             _appSettings = appsetting;
+            _mailHelper = mailHelper;
+            _userDetailService = userDetailService;
         }
 
-        [Authorize(Role.SuperAdmin,Role.Customer,Role.Admin,Role.Staff)]
+        [Authorize(Role.SuperAdmin, Role.Customer, Role.Admin, Role.Staff)]
         [HttpGet]
         [Route("ShowUsers")]
         public IActionResult ShowUser()
@@ -83,6 +91,65 @@ namespace RestApi.src.Controllers
                 Status = true,
                 Data = "Update"
             });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("registration")]
+        public async Task<IActionResult> Registration(Registration registration)
+        {
+            if (!CheckRegisterMailIsUse(registration.Mail))
+            {
+                return Ok(new RegistrationResponse()
+                {
+                    Status = false,
+                    Data = "Mail is registration"
+                });
+            }
+
+            await CreateUserDetail(registration,await CreateUser(registration));
+            
+            _mailHelper.SendMail(new Mailer()
+            {
+                MailTo = registration.Mail,
+                NameTo = registration.Name,
+                Subject = "test",
+                Content = "verify mail"
+            });
+            return Ok(new RegistrationResponse(){
+                Status = true,
+                Data = "Register Success"
+            });
+        }
+
+        private async Task CreateUserDetail(Registration registration,User user)
+        {
+            await _userDetailService.Insert(
+                new UserDetail()
+                {
+                    UserId = user.Id,
+                    City = registration.City,
+                    Country = registration.Country,
+                    Street = registration.Street
+                }
+            );
+        }
+
+        private async Task<User> CreateUser(Registration registration)
+        {
+            var user = new User()
+            {
+                Mail = registration.Mail,
+                Password = registration.Password,
+                Name = registration.Name
+            };
+            await _repository.Insert(user);
+            return user;
+        }
+
+        private bool CheckRegisterMailIsUse(string mail)
+        {
+            return (_repository.SearchUserMail(mail) != null) ? true : false;
         }
 
         [AllowAnonymous]
