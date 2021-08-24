5058a8c3b4b1fe528334dc69c4ea244a3903b588
jan <bossjien26@gmail.com>
2021-08-24 22:47:45 +0800

    feat:add required type

--- src/RestApi/Models/Requests/Registration.cs
+++ src/RestApi/Models/Requests/Registration.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RestApi.Models.Requests
 {
     public class Registration
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Mail { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
         public string Country { get; set; }
