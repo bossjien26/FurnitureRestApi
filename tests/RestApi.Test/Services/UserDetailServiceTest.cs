0aa3b4a48d1fb06b0b7cbc8d14e42bffa03ab065
jan <bossjien26@gmail.com>
2021-08-26 22:00:36 +0800

    test:add UserDetailServiceTest

---
+++ tests/RestApi.Test/Services/UserDetailServiceTest.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Entities;
+using Moq;
+using NUnit.Framework;
+using Repositories.IRepository;
+using Services.Service;
+using src.Repositories.IRepository;
+using src.Services.Service;
+
+namespace RestApi.Test.Services
+{
+    [TestFixture]
+    public class UserDetailServiceTest
+    {
+        private readonly Mock<UserDetail> _entityMock;
+
+        private readonly Mock<IUserDetailRepository> _repoMock;
+
+        private readonly Mock<User> _entityUserMock;
+
+        private readonly Mock<IUserRepository> _repoUserMock;
+
+        public UserDetailServiceTest()
+        {
+            _entityMock = new Mock<UserDetail>();
+
+            _repoMock = new Mock<IUserDetailRepository>();
+
+            _entityUserMock = new Mock<User>();
+
+            _repoUserMock = new Mock<IUserRepository>();
+        }
+
+        [Test]
+        public async Task ShouldInsertOne()
+        {
+            //Arrange
+            var userService = new UserService(_repoUserMock.Object);
+
+            //Act
+            await userService.Insert(_entityUserMock.Object);
+            _entityMock.Object.UserId = _entityUserMock.Object.Id;
+
+            //Assert
+            Assert.DoesNotThrowAsync(() =>
+                new UserDetailService(_repoMock.Object).Insert(_entityMock.Object)
+            );
+        }
+    }
+}
