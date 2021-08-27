22921ddbfdbe7a4b516c2acc3f987182c4b9d0fe
jan <bossjien26@gmail.com>
2021-08-27 21:12:30 +0800

    feat:add insert category method

--- src/RestApi/Controllers/CategoryController.cs
+++ src/RestApi/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
+using System.Threading.Tasks;
 using DbEntity;
+using Enum;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Middlewares.Authentication;
+using RestApi.src.Models;
 using Services.IService;
 using Services.Service;
 
@@ -19,6 +23,42 @@ namespace RestApi.Controllers
             _repository = new CategoryService(context);
 
             _logger = logger;
+        }
+
+        [Authorize(Role.SuperAdmin, Role.Admin)]
+        [Route("Insert")]
+        [HttpPost]
+        public async Task<IActionResult> Insert(Models.Requests.Category category)
+        {
+            if (await _repository.GetById(category.ChildrenId) == null && category.ChildrenId != 0)
+            {
+                return Ok(new AutResultModel()
+                {
+                    Status = false,
+                    Data = "Fail"
+                });
+            }
+
+            await InsertCategory(category);
+
+            return Ok(new AutResultModel()
+            {
+                Status = true,
+                Data = "Success"
+            });
+        }
+
+        private async Task InsertCategory(Models.Requests.Category category)
+        {
+            await _repository.Insert(
+                new Entities.Category()
+                {
+                    Name = category.Name,
+                    ChildrenId = category.ChildrenId,
+                    Sequence = category.Sequence,
+                    IsDisplay = category.IsDisplay
+                }
+            );
         }
     }
 }
