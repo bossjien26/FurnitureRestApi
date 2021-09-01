fd167975154df3301b3817dd02da60f97e9abd67
jan <bossjien26@gmail.com>
2021-09-01 21:35:16 +0800

    test:Category Repository

---
+++ tests/RestApi.Test/Repositories/CategoryRepositoryTest.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Repositories.IRepository;
+using Repositories.Repository;
+using RestApi.Test.DatabaseSeeders;
+
+namespace RestApi.Test.Repositories
+{
+    public class CategoryRepositoryTest : BaseRepositoryTest
+    {
+        private readonly ICategoryRepository _repository;
+
+        public CategoryRepositoryTest() =>
+        _repository = new CategoryRepository(_context);
+
+        [Test]
+        async public Task ShouldGet()
+        {
+            var seeder = CategorySeeder.SeedOne();
+            await _repository.Insert(seeder);
+            var product = _repository.Get(c => c.Id == seeder.Id);
+            Assert.IsNotNull(product);
+        }
+
+        [Test]
+        async public Task ShouldGetAll()
+        {
+            await _repository.InsertMany(CategorySeeder.SeedMany(5, 5));
+            var products = await _repository.GetAll().Take(5).ToListAsync();
+            Assert.IsNotNull(products);
+            Assert.AreEqual(5, products.Count);
+        }
+
+        [Test]
+        public void ShouldCreateError()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _repository.Insert(null));
+        }
+
+        [Test]
+        public async Task ShouldDelete()
+        {
+            var testData = CategorySeeder.SeedOne();
+            await _repository.Insert(testData);
+
+            Assert.DoesNotThrowAsync(() => _repository.Delete(testData));
+            Assert.IsNull(await _repository.Get(x => x.Id == testData.Id));
+        }
+
+        [Test]
+        public void ShouldDeleteError()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _repository.Delete(null));
+        }
+
+        [Test]
+        public async Task ShouldUpdate()
+        {
+            var testData = CategorySeeder.SeedOne();
+            await _repository.Insert(testData);
+
+            testData.Name = "Taiwain";
+            Assert.DoesNotThrowAsync(() => _repository.Update(testData));
+        }
+
+        [Test]
+        public void ShouldUpdateError()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _repository.Update(null));
+        }
+    }
+}
