d2609690560c4aa14c4e4c35459f773210289301
jan <bossjien26@gmail.com>
2021-09-01 23:02:05 +0800

    test:Specification Repository

---
+++ tests/RestApi.Test/DatabaseSeeders/SpecificationSeeder.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Bogus;
+using Bogus.Extensions;
+using Entities;
+
+namespace RestApi.Test.DatabaseSeeders
+{
+    public class SpecificationSeeder
+    {
+        public static Specification SeedOne() => SetSpecification().Generate();
+
+        public static List<Specification> SeedMany(int min , int max) => 
+                SetSpecification().GenerateBetween(min,max);
+
+        private static Faker<Specification> SetSpecification()
+        {
+            return new Faker<Specification>()
+                .RuleFor(c => c.Id, (f) => 0)
+                .RuleFor(c => c.Name, (f) => f.Random.AlphaNumeric(10))
+                .RuleFor(c => c.SpecificationContent, SetSpecificationContent());
+        }
+
+        private static Faker<SpecificationContent> SetSpecificationContent()
+        {
+            return new Faker<SpecificationContent>()
+                .RuleFor(c => c.Id, (f) => 0)
+                .RuleFor(c => c.Name, (f) => f.Random.AlphaNumeric(10))
+                .RuleFor(c => c.SpecificationId, (f) => f.Random.Number(10));
+        }
+    }
+}
