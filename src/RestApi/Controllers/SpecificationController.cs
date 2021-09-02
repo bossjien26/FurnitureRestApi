d3d1af07d265ef11ce400b36e04032d95a4eb121
jan <bossjien26@gmail.com>
2021-09-02 22:00:44 +0800

    feat:add Specification and SpecificationContent insert

--- src/RestApi/Controllers/SpecificationController.cs
+++ src/RestApi/Controllers/SpecificationController.cs
@@ -1,6 +1,12 @@
+using System.Threading.Tasks;
 using DbEntity;
+using Entities;
+using Enum;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Middlewares.Authentication;
+using RestApi.Models.Requests;
+using RestApi.src.Models;
 using Services.IService;
 using Services.Service;
 
@@ -23,6 +29,64 @@ namespace RestApi.Controllers
             _repositorySpecificationContent = new SpecificationContentService(context);
 
             _logger = logger;
+        }
+
+        [Authorize(Role.SuperAdmin, Role.Customer, Role.Admin, Role.Staff)]
+        [HttpPost]
+        [Route("Insert")]
+        public async Task<IActionResult> Insert(RequestSpecification requestSpecification)
+        {
+            await InsertSpecification(requestSpecification);
+
+            return Ok(new AutResultModel()
+            {
+                Status = true,
+                Data = "Success"
+            });
+        }
+
+        private async Task InsertSpecification(RequestSpecification requestSpecification)
+        {
+            await _repository.Insert(
+                new Specification()
+                {
+                    Name = requestSpecification.Name,
+                }
+            );
+        }
+
+        [Authorize(Role.SuperAdmin, Role.Customer, Role.Admin, Role.Staff)]
+        [HttpPost]
+        [Route("InsertUnderLayer")]
+        public async Task<IActionResult> InsertUnderLayer(RequestSpecificationContent requestSpecificationContent)
+        {
+            if (await _repositorySpecificationContent.GetById(requestSpecificationContent.SpecificationId) == null)
+            {
+                return Ok(new AutResultModel()
+                {
+                    Status = false,
+                    Data = "Fail"
+                });
+            }
+
+            await InsertSpecification(requestSpecificationContent);
+
+            return Ok(new AutResultModel()
+            {
+                Status = true,
+                Data = "Success"
+            });
+        }
+
+        private async Task InsertSpecification(RequestSpecificationContent requestSpecificationContent)
+        {
+            await _repositorySpecificationContent.Insert(
+                new SpecificationContent()
+                {
+                    Name = requestSpecificationContent.Name,
+                    SpecificationId = requestSpecificationContent.SpecificationId
+                }
+            );
         }
     }
 }
