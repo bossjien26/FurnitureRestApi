c4b4531c7a784ac12b55f5a1df278285de461a30
jan <bossjien26@gmail.com>
2021-09-03 21:21:35 +0800

    feat:add product insert

--- src/RestApi/Controllers/ProductController.cs
+++ src/RestApi/Controllers/ProductController.cs
@@ -1,13 +1,21 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using DbEntity;
+using Entities;
+using Enum;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Middlewares.Authentication;
+using RestApi.Models.Requests;
+using RestApi.src.Models;
 using Services.IService;
 using Services.Service;
 
 namespace RestApi.Controllers
 {
     [ApiController]
-    [Route('api/[Controller]')]
+    [Route("api/[Controller]")]
     public class ProductController : ControllerBase
     {
         private readonly IProductService _repository;
@@ -19,6 +27,41 @@ namespace RestApi.Controllers
             _repository = new ProductService(context);
 
             _logger = logger;
+        }
+
+        [Authorize(Role.SuperAdmin, Role.Admin)]
+        [Route("insert")]
+        [HttpPost]
+        public async Task<IActionResult> Insert(RequestProduct requestsProduct)
+        {
+            await InsertProdcut(requestsProduct);
+
+            return Ok(new AutResultModel()
+            {
+                Status = true,
+                Data = "Success"
+            });
+        }
+
+        private async Task InsertProdcut(RequestProduct requestProduct)
+        {
+            await _repository.Insert(new Product()
+            {
+                Name = requestProduct.Name,
+                Price = requestProduct.Price,
+                Sequence = requestProduct.Sequence,
+                Quantity = requestProduct.Quantity,
+                RelateAt = (requestProduct.RelateAt == null) ? DateTime.Now : requestProduct.RelateAt,
+                IsDisplay = requestProduct.IsDisplay
+            });
+        }
+
+        [Authorize(Role.SuperAdmin, Role.Admin, Role.Staff)]
+        [Route("many")]
+        [HttpGet]
+        public IActionResult ShowMany(int pages)
+        {
+            return Ok(_repository.GetMany(pages, 10).ToList());
         }
     }
 }
