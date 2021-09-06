b3898a7f0c6f4145c61dc61e0feaef92af40354a
jan <bossjien26@gmail.com>
2021-09-06 20:32:18 +0800

    feat:add RequestCart

---
+++ src/RestApi/Models/Requests/RequestCart.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Enum;
+
+namespace RestApi.Models.Requests
+{
+    public class RequestCart
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public CartAttribute Attribute { get; set; }
+    }
+}
