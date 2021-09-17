bbae8c2222b7146a2ca61e64a86edf45e0930c4a
jan <bossjien26@gmail.com>
2021-09-17 20:41:40 +0800

    test:add baseController

---
+++ tests/RestApi.Test/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestApi.Test.Repositories;
+
+namespace RestApi.Test.Controllers
+{
+    public class BaseController : BaseRepositoryTest
+    {
+        private readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWlsIjoiamFuQGV4YW1wbGUuY29tIiwicGFzc3dvcmQiOiJhYWFhYWFhIiwibmJmIjoxNjMxNzk2NjUwLCJleHAiOjE2MzI0MDE0NTAsImlhdCI6MTYzMTc5NjY1MH0.EByJ_a9FbVlcXbHmmWNdAXCeqqEKQda1tpvk9_6CGco";
+
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        protected HttpClient _httpClient;
+
+        public BaseController()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+            _httpClient = _factory.WithWebHostBuilder(builder =>
+                { }).CreateClient(new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false,
+                });
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+        }
+    }
+}
