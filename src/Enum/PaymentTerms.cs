971907ad5c1fc314b900ab499753b256cfe0d8e7
jan <bossjien26@gmail.com>
2021-09-22 23:06:09 +0800

    style:add comment

--- src/Enum/PaymentTerms.cs
+++ src/Enum/PaymentTerms.cs
@@ -5,8 +5,17 @@ namespace Enum
     /// </summary>
     public enum PaymentTerms
     {
+        /// <summary>
+        /// 銀行存款
+        /// </summary>
         Bank = 1,
+        /// <summary>
+        /// 信用卡
+        /// </summary>
         Credit = 2,
+        /// <summary>
+        /// 貨到付款
+        /// </summary>
         COD = 3
     }
 }
