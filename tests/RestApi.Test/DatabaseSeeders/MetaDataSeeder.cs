2b5ce5ea77c641fcadd7c7bffad1a9977099f2bf
jan <bossjien26@gmail.com>
2021-09-23 22:47:50 +0800

    test:add metaData service

---
+++ tests/RestApi.Test/DatabaseSeeders/MetaDataSeeder.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Bogus;
+using Bogus.Extensions;
+using Entities;
+
+namespace RestApi.Test.DatabaseSeeders
+{
+    public class MetaDataSeeder
+    {
+        public static MetaData SeedOne() => SetMetaData().Generate();
+
+        public static List<MetaData> SeedMany(int min, int max) =>
+            SetMetaData().GenerateBetween(min, max);
+
+        private static Faker<MetaData> SetMetaData()
+        {
+            return new Faker<MetaData>()
+            .RuleFor(c => c.Key, (f) => f.Random.AlphaNumeric(10))
+            .RuleFor(c => c.Type, (f) => f.Random.AlphaNumeric(10))
+            .RuleFor(c => c.Value, (f) => f.Random.AlphaNumeric(10));
+        }
+    }
+}
