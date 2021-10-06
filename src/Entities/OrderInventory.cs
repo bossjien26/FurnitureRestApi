01807ee1f8e4bf6926c22a1412b5f8d6e942afba
jan <bossjien26@gmail.com>
2021-10-06 21:18:47 +0800

    style:remove comment

--- src/Entities/OrderInventory.cs
+++ src/Entities/OrderInventory.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities
 {
-    //TODO:rename to OrderInventory
     [Table("OrderInventory")]
     public class OrderInventory
     {
