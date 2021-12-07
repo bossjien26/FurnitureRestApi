ed91cfd8181e8fe5acf43c2aed18b05372683df3
jan <bossjien26@gmail.com>
2021-12-07 23:48:42 +0800

    feat:update get next specification

--- src/RestApi/Controllers/ProductSpecificationController.cs
+++ src/RestApi/Controllers/ProductSpecificationController.cs
@@ -48,14 +48,25 @@ namespace RestApi.Controllers
         public IActionResult Detail(GetProductSpecificationRequest getProductSpecification)
         {
             var specificationIds = getProductSpecification.Specifications.ToList();
+            var basicSpecifications = new List<InventoryIdBySpecifications>();
+            _service.GetManyJoinSpecification(getProductSpecification.ProductId).ToList().ForEach(r => {
+                basicSpecifications.Add(new InventoryIdBySpecifications(){
+                    Id = r.Id,
+                    Name = r.Name
+                });
+            });
             var specificationContents = _service.GetBySpecificationContent(getProductSpecification.ProductId, specificationIds).ToList();
             var inventoryIds = _service.GetOneJoinSpecificationByProductId(getProductSpecification.ProductId
                 , specificationIds).ToList();
             var nextSpecification = _service.GetByNextSpecification(getProductSpecification.ProductId, specificationIds.LastOrDefault()).FirstOrDefault();
+            if(nextSpecification == null)
+            {
+                return Ok("no specification");
+            }
             var nextInventorySpecification = _service.GetByInventoryIds(inventoryIds, nextSpecification.Id).ToList();
             nextInventorySpecification.RemoveAll(x => specificationContents.Where(z => z.InventoryIdBySpecificationContent.Id == x.InventoryIdBySpecificationContent.Id).Any());
             specificationContents.AddRange(nextInventorySpecification);
-
+            specificationContents.AddRange(basicSpecifications);
             var specifications = specificationContents.GroupBy(x => x.Id).ToDictionary(x => x.Key, x => x.ToList());
             return Ok(TransferSpecification(specifications).ToList());
         }
@@ -68,12 +79,14 @@ namespace RestApi.Controllers
                 var specificationContentResponses = new List<SpecificationContentResponse>();
                 specification.Value.ForEach(x =>
                 {
-                    specificationContentResponses.Add(
-                    new SpecificationContentResponse()
-                    {
-                        Id = x.InventoryIdBySpecificationContent.Id,
-                        Name = x.InventoryIdBySpecificationContent.Name
-                    });
+                    if(x.InventoryIdBySpecificationContent.Name != null){
+                        specificationContentResponses.Add(
+                        new SpecificationContentResponse()
+                        {
+                            Id = x.InventoryIdBySpecificationContent.Id,
+                            Name = x.InventoryIdBySpecificationContent.Name
+                        });
+                    }
                 });
                 specificationResponses.Add(specification.Key, new SpecificationResponse()
                 {
