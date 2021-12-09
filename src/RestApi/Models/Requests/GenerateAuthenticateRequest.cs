c870e138b0105b9b5c19ecc32cac6f34137efe21
jan <bossjien26@gmail.com>
2021-12-09 22:01:19 +0800

    feat:remove no use column

--- src/RestApi/Models/Requests/GenerateAuthenticateRequest.cs
+++ src/RestApi/Models/Requests/GenerateAuthenticateRequest.cs
@@ -4,8 +4,6 @@ namespace RestApi.Models.Requests
 {
     public class GenerateAuthenticateRequest
     {
-        public int Id { get; set; }
-
         [Required]
         public string Mail { get; set; }
         
