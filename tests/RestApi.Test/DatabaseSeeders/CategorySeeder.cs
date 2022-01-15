e8a500daf00f2fbfcbb5e745b1990a8a9c610c50
jan <bossjien26@gmail.com>
2022-01-15 12:29:52 +0800

    feat:change get children category

--- tests/RestApi.Test/DatabaseSeeders/CategorySeeder.cs
+++ tests/RestApi.Test/DatabaseSeeders/CategorySeeder.cs
@@ -7,18 +7,29 @@ namespace RestApi.Test.DatabaseSeeders
 {
     public class CategorySeeder
     {
-        public static Category SeedOne() => SetCategory().Generate();
+        public static Category SeedOne() => SetParentCategory().Generate();
 
         public static List<Category> SeedMany(int min, int max) =>
-            SetCategory().GenerateBetween(min, max);
+            SetParentCategory().GenerateBetween(min, max);
 
-        private static Faker<Category> SetCategory()
+        private static Faker<Category> SetParentCategory()
         {
             return new Faker<Category>()
                 .RuleFor(c => c.Id, (f) => 0)
                 .RuleFor(c => c.Name, (f) => f.Random.AlphaNumeric(10))
-                .RuleFor(c => c.ParentId, (f) => 0)
+                .RuleFor(c => c.ParentId, (f) => null)
+                .RuleFor(c => c.Sequence, (f) => f.Random.Number(10))
+                .RuleFor(c => c.ChildrenCategories, (f) => SetChildrenCategory().Generate(2));
+        }
+
+        private static Faker<Category> SetChildrenCategory()
+        {
+            return new Faker<Category>()
+                .RuleFor(c => c.Id, (f) => 0)
+                .RuleFor(c => c.Name, (f) => f.Random.AlphaNumeric(10))
+                .RuleFor(c => c.ParentId, (f) => f.Random.Number(10))
                 .RuleFor(c => c.Sequence, (f) => f.Random.Number(10));
         }
+
     }
 }
