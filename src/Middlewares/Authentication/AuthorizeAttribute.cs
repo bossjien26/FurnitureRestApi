ee87420f6801bddfa9aa959fb1fd50a9c907b152
jan <bossjien26@gmail.com>
2022-01-22 22:53:24 +0800

    feat:change check role is exist

--- src/Middlewares/Authentication/AuthorizeAttribute.cs
+++ src/Middlewares/Authentication/AuthorizeAttribute.cs
@@ -16,18 +16,18 @@ namespace Middlewares.Authentication
 
         public AuthorizeAttribute(params RoleEnum[] roles)
         {
-            _roles = roles ?? new RoleEnum[] {};
+            _roles = roles ?? new RoleEnum[] { };
         }
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            // skip authorization if action is decorated with [AllowAnonymous] attribute
+            // skip authorization if action is decorated withp [AllowAnonymous] attribute
             var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
             if (allowAnonymous)
                 return;
-                
+
             var user = (User)context.HttpContext.Items["User"];
-            if (user == null || (_roles.Any() && !_roles.Contains((user.Role))))
+            if (user == null || !_roles.Contains((user.Role)))
             {
                 // not logged in
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
