fc81496b7856799a27d7639f0bda52340ea1e3e8
jan <bossjien26@gmail.com>
2022-01-29 00:39:30 +0800

    feat:add order status service

---
+++ src/Services/Interface/IOrderStatusesService.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Services.Interface
+{
+    public interface IOrderStatusesService
+    {
+        Task Insert(OrderStatuses orderStatuses);
+
+        Task<OrderStatuses> GetByOrderId(int orderId);
+
+        IQueryable<OrderStatuses> GetManyByOrderId(int orderId);
+    }
+}
---
+++ src/Services/OrderStatusesService.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DbEntity;
+using Entities;
+using Repositories;
+using Repositories.Interface;
+using Services.Interface;
+
+namespace Services
+{
+    public class OrderStatusesService : IOrderStatusesService
+    {
+        private readonly IOrderStatusesRepository _repository;
+
+        public OrderStatusesService(DbContextEntity contextEntity)
+        => _repository = new OrderStatusesRepository(contextEntity);
+
+        public OrderStatusesService(IOrderStatusesRepository genericRepository)
+            => _repository = genericRepository;
+
+        public async Task Insert(OrderStatuses orderStatuses)
+        => await _repository.Insert(orderStatuses);
+
+        public async Task<OrderStatuses> GetByOrderId(int orderId)
+        => await _repository.Get(x => x.OrderId == orderId);
+
+        public IQueryable<OrderStatuses> GetManyByOrderId(int orderId)
+        => _repository.GetAll().Where(x => x.OrderId == orderId);
+    }
+}
