e08b4c68b1b88979c7a62693efe256b3303e8294
jan <bossjien26@gmail.com>
2022-01-28 22:24:27 +0800

    feat:add required

--- src/RestApi/Models/Requests/CreateOrderPayRequest.cs
+++ src/RestApi/Models/Requests/CreateOrderPayRequest.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using Enum;
 
 namespace RestApi.Models.Requests
 {
     public class CreateOrderPayRequest
     {
+        [Required]
         public int orderId { get; set; }
 
+        [Required]
         public PaymentTypeEnum Terms { get; set; }
     }
 }
