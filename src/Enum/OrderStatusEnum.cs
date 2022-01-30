8fa226de713d4304bc36da621b722bd89d925b0f
jan <bossjien26@gmail.com>
2022-01-30 13:16:01 +0800

    feat:add order status enum

--- src/Enum/OrderStatusEnum.cs
+++ src/Enum/OrderStatusEnum.cs
@@ -2,13 +2,14 @@ namespace Enum
 {
     public enum OrderStatusEnum
     {
-        Cancel = 1,
-        Refund = 2,
-        Return = 3,
-        Receiver = 4,
-        Paid = 5,
-        Shipped = 6,
-        Processing = 7,
-        Error = 8
+        Created = 1,
+        Cancel = 2,
+        Refund = 3,
+        Return = 4,
+        Receiver = 5,
+        Paid = 6,
+        Shipped = 7,
+        Processing = 8,
+        Error = 9
     }
 }
