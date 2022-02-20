af24ec22b06865561409de3ba075fb7a80ae74b5
jan <bossjien26@gmail.com>
2022-02-20 15:42:06 +0800

    feat:add Required

--- src/RestApi/Models/Requests/UpdateUserDetailRequest.cs
+++ src/RestApi/Models/Requests/UpdateUserDetailRequest.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RestApi.Models.Requests
 {
     public class UpdateUserDetailRequest
     {
+        [Required]
         public string City { get; set; }
 
+        [Required]
         public string Country { get; set; }
 
+        [Required]
         public string Street { get; set; }
     }
 }
